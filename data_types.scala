9e62df4e96fdff42f18a1faf005752f3e03e0f66
olaolaabim <117292918+olaolaabim@users.noreply.github.com>
2022-11-04 10:47:26 +0000

    Add files via upload

---
+++ data_types.scala
@@ -0,0 +1,22 @@
+object data_types {
+  def main(args: Array[String]): Unit = {
+    var a: Boolean = true
+    var a1: Byte = 126
+    var a2: Float = 2.45673f
+    var a3: Int = 3
+    var a4: Short = 45
+    var a5: Double = 2.93846523
+    var a6: Char = 'A'
+    var a7: String = "Hello World"
+
+    println("boolean:" +a)
+    println("byte:" + a1)
+    println("float:" + a2)
+    println("integer:" + a3)
+    println("short:" + a4)
+    println("double:" + a5)
+    println("char:" + a6)
+    println("string:" + a7)
+  }
+
+}
