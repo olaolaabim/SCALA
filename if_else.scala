7e3fa0c7f7cbb1a4f27b598f6bad898150221161
olaolaabim <117292918+olaolaabim@users.noreply.github.com>
2022-11-04 11:01:46 +0000

    Add files via upload

---
+++ if_else.scala
@@ -0,0 +1,16 @@
+object if_else {
+  def main(args: Array[String]): Unit = {
+    val x = 20;
+    val y = 30;
+    var res =""
+    if (x == 20 && y == 30) {
+      res = "x == 20 && y == 30";
+    }
+    else {
+      res = "x != 20 && y != 30 ";
+    }
+    println(res)
+    val res2 = if (x != 20) "x==20" else "x !=20"
+    print(res2)
+  }
+}
